0e9c332e7aadaf770b90cba6b1f1fda66fa2b641
Ivan Perehiniak <iv.perehinik@gmail.com>
2021-01-08 23:52:26 +0100

    Update

---
+++ FT_FIFO_Terminal/FT_FIFO_Example/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FT_FIFO_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.RxCheckTimer = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SCAN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OPEN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 26);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "CLOSE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(175, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(319, 283);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "CLEAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // RxCheckTimer
+            // 
+            this.RxCheckTimer.Tick += new System.EventHandler(this.RxCheckTimer_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(173, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(321, 95);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(155, 26);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "SEND";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 308);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(155, 26);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "RECEIVING";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Serial Number";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 76);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(143, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 106);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DEVICE FILTER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Descriptor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 410);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FT FIFO Terminal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer RxCheckTimer;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FT_FIFO_Terminal/FT_FIFO_Example/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FTD2XX_NET;
+
+
+namespace FT_FIFO_Example
+{
+    public partial class Form1 : Form
+    {
+
+        UInt32 ftdiDeviceCount = 0;
+        FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;
+        // Create new instance of the FTDI device class
+        FTDI myFtdiDevice = new FTDI();
+
+        int deviceIndex = -1;
+
+        // Allocate storage for device info list
+        FTDI.FT_DEVICE_INFO_NODE[] ftdiDeviceList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ftdiDeviceList = new FTDI.FT_DEVICE_INFO_NODE[5];
+            RxCheckTimer.Enabled =  false;
+            textBox4.Text = "Logic";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text =  "";
+            // Determine the number of FTDI devices connected to the machine
+            ftStatus = myFtdiDevice.GetNumberOfDevices(ref ftdiDeviceCount);
+            // Check status
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                textBox1.Text += "Number of FTDI devices: " + ftdiDeviceCount.ToString()+ "\r\n";
+            }
+            else 
+            {
+                // Wait for a key press
+                textBox1.Text += "Failed to get number of devices (error " + ftStatus.ToString() + ")\r\n";
+                return;
+            }
+
+            // If no devices available, return
+            if (ftdiDeviceCount == 0)
+            {
+                // Wait for a key press
+                textBox1.Text += "Failed to get number of devices (error " + ftStatus.ToString() + ")\r\n";
+                return;
+            }
+
+            // Populate our device list
+            ftStatus = myFtdiDevice.GetDeviceList(ftdiDeviceList);
+
+            if (ftStatus == FTDI.FT_STATUS.FT_OK)
+            {
+                for (UInt32 i = 0; i < ftdiDeviceCount; i++)
+                {
+                    string tempDescription = ftdiDeviceList[i].Description.ToString();
+                    string tempSerial = ftdiDeviceList[i].SerialNumber.ToString();
+                    if (tempDescription.Contains(textBox4.Text) && tempSerial.Contains(textBox3.Text))
+                    {
+                        deviceIndex = (int)i;
+                        textBox1.Text += "Device Index: " + i.ToString() + "\r\n";
+                        textBox1.Text += "Flags: " + String.Format("{0:x}", ftdiDeviceList[i].Flags) + "\r\n";
+                        textBox1.Text += "Type: " + ftdiDeviceList[i].Type.ToString() + "\r\n";
+                        textBox1.Text += "ID: " + String.Format("{0:x}", ftdiDeviceList[i].ID) + "\r\n";
+                        textBox1.Text += "Location ID: " + String.Format("{0:x}", ftdiDeviceList[i].LocId) + "\r\n";
+                        textBox1.Text += "Serial Number: " + ftdiDeviceList[i].SerialNumber.ToString() + "\r\n";
+                        textBox1.Text += "Description: " + ftdiDeviceList[i].Description.ToString() + "\r\n\r\n";
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (deviceIndex >= 0)
+            {
+                ftStatus = myFtdiDevice.OpenBySerialNumber(ftdiDeviceList[deviceIndex].SerialNumber);
+                if (ftStatus != FTDI.FT_STATUS.FT_OK)
+                {
+                    // Wait for a key press
+                    textBox1.Text += "Failed to open device (error " + ftStatus.ToString() + ")\r\n";
+                    return;
+                }
+                else textBox1.Text += "Device opened !!!\r\n";
+            }
+
+            ftStatus = myFtdiDevice.SetBitMode(0x00, 0x40);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                textBox1.Text += "Failed to set bit mode (error " + ftStatus.ToString() + ")\r\n";
+                return;
+            }
+            else textBox1.Text += "Bit mode = 0x40 !!!\r\n";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ftStatus = myFtdiDevice.Close();
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                // Wait for a key press
+                textBox1.Text += "Failed to close device (error " + ftStatus.ToString() + ")\r\n";
+                return;
+            }
+            else textBox1.Text += "Device closed !!!" + "\r\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void RxCheckTimer_Tick(object sender, EventArgs e)
+        {
+            RxCheckTimer.Enabled = false;
+            uint RxCount = 0;
+            var rx_buf = new byte[1000];
+            string rx_str_buf = "";
+            uint data_read = 0;
+            ftStatus = myFtdiDevice.GetRxBytesAvailable(ref RxCount);
+            if (ftStatus != FTDI.FT_STATUS.FT_OK)
+            {
+                textBox1.Text = "<<ERROR: " + ftStatus.ToString() + "\r\n";
+            }
+            else if (RxCount > 0)
+            {
+                ftStatus = myFtdiDevice.Read(rx_buf, RxCount, ref data_read);
+                //rx_str_buf = System.Text.Encoding.Default.GetString(rx_buf);
+                rx_str_buf = BitConverter.ToString(rx_buf);
+                textBox1.Text += "Received " + RxCount + " bytes   <<" + rx_str_buf + ">>\r\n\r\n";
+            }
+            else
+            {
+                //textBox1.Text += "<> " + RxCount + " <>" + "\r\n";
+            }
+            RxCheckTimer.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bool timerState = RxCheckTimer.Enabled;
+            RxCheckTimer.Enabled = false;
+            var tx_buf = new byte[1000];
+            int tx_length = 0;
+            for (int i = 0; i < 998; i++)
+            {
+                tx_buf[i] = 0x00;
+            }
+            for (int i = 0; i < textBox2.Text.Length; i++)
+            {
+                if (i < 999)
+                {
+                    tx_buf[i] = (byte)textBox2.Text[i];
+                    tx_length = i + 1;
+                }
+            }
+            uint data_written = 0;
+            ftStatus = myFtdiDevice.Write(tx_buf, tx_length, ref data_written);
+            System.Threading.Thread.Sleep(200);
+            textBox1.Text = data_written + " bytes send\r\n";
+            RxCheckTimer.Enabled = timerState;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (RxCheckTimer.Enabled == true)
+            {
+                RxCheckTimer.Enabled = false;
+                button6.BackColor = Color.Transparent;
+            }else
+            {
+                RxCheckTimer.Enabled = true;
+                button6.BackColor = Color.Green;
+            }
+        }
+    }
+}
